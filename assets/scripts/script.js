85379730d9282148717bea3e1a2f1312799b78e4
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-08-30 19:34:28 -0300

    @Lucas-Reluz [Task: Feito login e customizado, falta js] {19:34| 30/08/2022}

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+function acao(){
+    let imgLogin = document.querySelector('.Login')
+    imgLogin.style.display = 'none';
+    imgLogin.classList.toggle("fade")
+
+    if(imgLogin = ".Login"){
+        let login = document.querySelector('.formLogin')
+
+        login.style.display = 'flex'
+    }
+}
